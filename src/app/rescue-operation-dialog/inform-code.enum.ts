657c22252dfd0a83940eb323e405766a4dc8a9b5
christoph <christoph.leitner@redlink.at>
2023-12-29 00:46:36 +0100

    clean up code

--- src/app/rescue-operation-dialog/inform-code.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum InformCodeEnums {
-    S = "01 Stationäre Behandlung",
-    A = "02 Ambulante Behandlung",
-}
--- src/app/rescue-operation-dialog/transport-type.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum TransportTypeEnums {
-    "01" = "01 Transport Krankenhaus",
-    "02" = "02 Transport Ambulanz",
-}
